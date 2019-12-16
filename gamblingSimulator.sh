0e2b0fd13bf173ab3fa06c318b732ee8d694c9f3
aniketchahare <anichahare@gmail.com>
2019-12-16 15:11:19 +0530

    Added: total amount win or loss

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,25 +1,43 @@
 #!/bin/bash -x
 
 echo "Welcome to Gambilng"
+declare -A gameRecord
+
 
 #constant
 BET=1
 STAKE=100
-LOW_LIMIT=$(( $STAKE / 2 ))
-HIGH_LIMIT=$(($STAKE / 2 + $STAKE))
+DAYS=20
 
 function dailyResult(){
+for(( day=1; day<=$DAYS; day++))
+do
+	STAKE=100
+	win=0
+	loss=0
+	LOW_LIMIT=$(( $STAKE / 2 ))
+	HIGH_LIMIT=$(($STAKE / 2 + $STAKE))
+
 	while [[ $LOW_LIMIT -lt $STAKE && $HIGH_LIMIT -gt $STAKE ]]
 	do
 		winLossResult=$((RANDOM%2))
 		if [ $winLossResult -eq 1 ]
 		then
 			STAKE=$(($STAKE+1))
+			win=$(($win+1))
 		else
 			STAKE=$(($STAKE-1))
+			loss=$(($loss-1))
 		fi
 	done
-	echo "Stake:- "$STAKE
+	outCome=$(( $STAKE - 100 ))
+	gameRecord["Day_$day"]=$win" "$loss
+	done
 }
 
 dailyResult
+
+for i in ${!gameRecord[@]}
+do
+	echo "$i ${gameRecord[$i]}"
+done | sort -k2 -n
